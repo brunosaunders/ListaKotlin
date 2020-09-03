e404facf437da09fa63cfe9fb7a315e70f5be394
Bruno Saunders <senabruno97@hotmail.com>
2020-09-02 21:25:56 -0300

    adding the exercies project

---
+++ src/br/digital/com/aulakotlinsemobjeto/Main.kt
@@ -0,0 +1,12 @@
+package br.digital.com.aulakotlinsemobjeto
+
+
+fun main() {
+    showTheFirst100PositiveNumbers()
+}
+
+fun showTheFirst100PositiveNumbers() {
+    for (item in 1..100) {
+        println(item)
+    }
+}
