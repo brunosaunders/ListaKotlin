6b4c1b6331f4c93efb0950954439cf3ee885476a
Victor Rubens <victorrss@live.com>
2020-09-02 21:36:01 -0300

    exercicio 4

--- src/br/digital/com/aulakotlinsemobjeto/Main.kt
+++ src/br/digital/com/aulakotlinsemobjeto/Main.kt
@@ -3,10 +3,23 @@ package br.digital.com.aulakotlinsemobjeto
 
 fun main() {
     showTheFirst100PositiveNumbers()
+
+    exercicio4() // Victor Rubens
 }
 
 fun showTheFirst100PositiveNumbers() {
     for (item in 1..100) {
         println(item)
+    }
+}
+
+// Victor Rubens
+fun exercicio4() {
+    print("Exercício 4: Os primeiros 100 números inteiros positivos ímpares: ")
+    var i = 1
+    while (i <= 200) {
+        if (i % 2 != 0)
+            print("$i,")
+        i++
     }
 }
